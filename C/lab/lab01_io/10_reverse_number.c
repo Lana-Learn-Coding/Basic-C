f29c88e198e6240f8b8dd16d89464eb2f55c92ce
Lana <sitdownrightnow2552@gmail.com>
2019-08-31 00:29:04 +0700

    refactor
    specified the digits of the number

--- C/lab/lab01_io/10_reverse_number.c
+++ C/lab/lab01_io/10_reverse_number.c
@@ -3,14 +3,14 @@
 
 int main(int argc, char const *argv[])
 {
-	int number;
+	int twoDigitsNumber;
 	int unitPosition, tensPosition;
 
 	printf("nhap so co 2 chu so can dao nguoc\n");
-	scanf("%d", &number);
+	scanf("%d", &twoDigitsNumber);
 
-	unitPosition = number % 10;
-	tensPosition = number / 10;
+	unitPosition = twoDigitsNumber % 10;
+	tensPosition = twoDigitsNumber / 10;
 
 	printf("so da dao nguoc la %d%d", unitPosition, tensPosition);
 
