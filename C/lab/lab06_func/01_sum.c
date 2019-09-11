868d71300cfe5f986eac158e534b853ead6a0a8e
Lana <sitdownrightnow2552@gmail.com>
2019-09-11 19:36:41 +0700

    added

---
+++ C/lab/lab06_func/01_sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(int argc, char const *argv[])
+{
+	int a, b;
+	printf("nhap 2 so a b: ");
+	scanf("%d %d", &a, &b);
+
+	printf("tong 2 so %d + %d la %d", a, b, sum(a, b));
+	getch();
+	return 0;
+}
+
+int sum(int a, int b) {
+	return a + b;
+}
